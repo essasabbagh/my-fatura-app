b950a3d57e8d63f9b76b47d71c2d03dea65f141e
essasabbagh <essasabbagh@gmail.com>
2021-02-09 21:01:52 +0300

    app fix

--- src/App.vue
+++ src/App.vue
@@ -6,24 +6,6 @@
     />
     <router-view name="sideBar" class="row-span-5 col-span-1 max-w-max" />
 
-    <transition name="fade">
-      <router-view class="row-span-5 col-span-11 col-start-2" />
-    </transition>
+    <router-view class="row-span-5 col-span-11 col-start-2" />
   </div>
 </template>
-
-<script>
-
-export default {
-};
-</script>
-
-<style>
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 0.5s;
-}
-.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
-  opacity: 0;
-}
-</style>
--- src/components/sideBar.vue
+++ src/components/sideBar.vue
@@ -9,7 +9,7 @@
           class="h-16 px-6 flex justify-center items-center w-full hover:bg-teal-200
 					focus:text-teal-500"
         >
-          <i class="fas fa-home"></i>
+          <i class="fas fa-chart-line"></i>
         </a>
       </li>
       <li class="hover:bg-gray-100">
@@ -18,16 +18,7 @@
           class="h-16 px-6 flex justify-center items-center w-full hover:bg-teal-200
 					focus:text-teal-500"
         >
-          <i class="far fa-calendar-alt"></i>
-        </router-link>
-      </li>
-      <li class="hover:bg-gray-100">
-        <router-link
-          to="/details"
-          class="h-16 px-6 flex justify-center items-center w-full hover:bg-teal-200
-					focus:text-teal-500"
-        >
-          <i class="fas fa-info-circle"></i>
+          <i class="fas fa-chart-bar"></i>
         </router-link>
       </li>
       <li class="hover:bg-gray-100">
@@ -36,45 +27,41 @@
           class="h-16 px-6 flex justify-center items-center w-full hover:bg-teal-200
 					focus:text-teal-500"
         >
-          <i class="fas fa-percentage"></i>
+          <i class="fas fa-chart-pie"></i>
         </router-link>
       </li>
-      
-      
-      
-
-      <!-- <li class="hover:bg-gray-100">
-        <a
-          href="#"
-          class="h-16 px-6 flex justify-center items-center w-full
+      <li class="hover:bg-gray-100">
+        <router-link
+          to="/details"
+          class="h-16 px-6 flex justify-center items-center w-full hover:bg-teal-200
 					focus:text-teal-500"
         >
-          <i class="fas fa-bell"></i>
-        </a>
-      </li> -->
+          <i class="fas fa-table"></i>
+        </router-link>
+      </li>
     </ul>
 
-    <div  class="mt-auto flex flex-col items-center w-full">
+    <div class="mt-auto flex flex-col items-center w-full">
       <router-link
-      v-if="true"
+        v-if="true"
         to="/register"
         class="h-16 px-6 flex justify-center items-center w-full hover:bg-green-300
-					focus:text-teal-500"
+					focus:text-green-500"
       >
         <i class="fas fa-sign-in-alt"></i>
       </router-link>
       <router-link
-      v-if="true"
+        v-if="true"
         to="/setting"
-        class="h-16 w-10 mx-auto flex justify-center items-center
-				w-full focus:text-red-500 hover:bg-yellow-300 focus:outline-none"
+        class="h-16 mx-auto flex justify-center items-center
+				w-full focus:text-yellow-500 hover:bg-yellow-300 focus:outline-none"
       >
         <i class="fas fa-cogs"></i>
       </router-link>
       <router-link
-      v-if="true"
+        v-if="true"
         to="/login"
-        class="h-16 w-10 mx-auto flex justify-center items-center
+        class="h-16 mx-auto flex justify-center items-center
 				w-full focus:text-red-500 hover:bg-red-300 focus:outline-none"
       >
         <i class="fas fa-sign-out-alt"></i>
