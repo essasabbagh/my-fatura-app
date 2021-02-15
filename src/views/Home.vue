9d3bcf8e66cef3a59e62a9da3a985dcef233be93
essasabbagh <essasabbagh@gmail.com>
2021-02-15 10:49:56 +0300

    home setup

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,18 @@
 <template>
-  <div class="">
-    <h1 class="text-4xl mt-5">Home:</h1>
+  <div class="text-xl">
+    <h1 class="text-4xl mt-5">Welcome:</h1>
+    <p class="my-5">
+      <strong>My Fatura</strong> is the best application to organize your bills
+      and view all details visually using charts
+    </p>
+    <p>In this application I used :</p>
+    <ul class="ml-5">
+      <li class="my-2"><strong>Vuejs: </strong>SPA Framwork,including Vue-Router and Vuex</li>
+      <li class="my-2"><strong>Tailwindcss: </strong>For styling and color</li>
+      <li class="my-2"><strong>Postcss7: </strong>To remove unused classes</li>
+      <li class="my-2"><strong>Font Awesome: </strong>For Icons</li>
+      <li class="my-2"><strong>ApexChart: </strong>Best tool for data visualization</li>
+      <li class="my-2"><strong>Firebase: </strong>For Hosting ,Cloud database ,and Authentication</li>
+    </ul>
   </div>
 </template>
