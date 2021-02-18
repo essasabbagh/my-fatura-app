efc57bcf1444229f278d5dabb3f794d27552edd4
essasabbagh <essasabbagh@gmail.com>
2021-02-18 19:04:38 +0300

    profile fix

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -62,7 +62,7 @@
               />
               <i
                 @click="$refs.profileImage.click()"
-                class="fas fa-upload absolute top-1/4 left-1/3 text-3xl z-10 text-white shadow-2xl opacity-70 invisible "
+                class="fas fa-upload absolute text-3xl z-10 text-white shadow-2xl opacity-70 invisible "
               ></i>
               <img
                 class="object-cover w-full h-full"
@@ -122,8 +122,13 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 .avatar:hover i {
   visibility: visible;
+}
+.fa-upload {
+  border: none;
+  top: 20px;
+  right: -15px;
 }
 </style>
